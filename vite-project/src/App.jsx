7de8f13f3e947c647f2ff04f10de2f978033075c
KESAVARAJJ <kesavarajj04@gmail.com>
2025-03-30 10:56:00 +0530

    Rectify the correction in calling the file

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+import TodoForm  from "./Pages/TodoForm.jsx"
+function App() {
+    return (
+      <>
+        <TodoForm />
+      </>
+    );
+  }
+  
+  export default App;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Header from './Components/Header.jsx'
-import TodoForm  from "./Pages/TodoForm.jsx"
+import App from "./App.jsx";
+import "./index.css";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="209830235983-9qv70em8v6mjhvrhct11o7aish797qfq.apps.googleusercontent.com">
     <Header />
-    <TodoForm />
+    <App />
     </GoogleOAuthProvider>
   </StrictMode>,
 )
